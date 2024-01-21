007f396ee08af4ea24ecac554fcf71dc9cb1e076
cho-sohyun <shteam1107@gmail.com>
2024-01-21 18:59:02 +0900

    🎉 초기 세팅

---
+++ src/components/TodoList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function TodoList() {
+  return (
+    <div>
+      <div className="flex justify-center items-center min-h-screen">
+        <div className="w-[512px] h-[768px] relative bg-white">오늘</div>
+      </div>
+    </div>
+  );
+}
+
+export default TodoList;
